ffd7c70a69f8e68122307731908b09c73dd7a8c8
Saim <146630631+saim-61s@users.noreply.github.com>
2025-05-14 03:49:18 +0000

    conversionOperation

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = undefined;
+// "dlkfj" changes to NaN in Number datatypes
+// null changes to 0 in Number datatypes
+// undefined  chnages to NaN in Number datatypes
+// true changes to 1
+console.log(typeof score)
+
+let valueIntNumber = Number(score)
+console.log(valueIntNumber)
+console.log(typeof valueIntNumber)
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let num = 434
+let str = String(num)
+console.log(str)
+console.log(typeof str)
