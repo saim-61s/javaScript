3bf85876403f8b2f7536810eb0aae4b24dd7a33f
Saim <146630631+saim-61s@users.noreply.github.com>
2025-05-14 03:28:33 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strick";  // treat all JS  code as newer version
+
+// alert(3+3) we are using nodejs, not browser
+
+console.log(3+3)
+console.log("samail")
+
+
+let name = "samail"
+let age = 18
+let isLoggedIn = false
+
+//number
+//bitint
+//string = " "
+//boolean = true, false
+// null = standalone value
+// undefined = 
+// symbol => unique
+
+
+//object
+
+console.log(typeof "samail")
+console.log(typeof null)
+console.log(typeof undefined)
